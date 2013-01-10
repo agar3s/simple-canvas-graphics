2159b213ed033f174cb9ba93a23c33fc4ee23e2c
Agares <ekeisco@gmail.com>
2013-01-10 12:55:07 -0500

    init version migrated from my first experience with html5 canvas

---
+++ simple-graphics.js
@@ -0,0 +1,61 @@
+var context = null;
+
+var init = function(context){
+    this.context = context;
+}
+
+var drawLine = function(iniX, iniY, finX, finY){
+    context.beginPath();
+    context.moveTo(iniX,iniY);
+    context.lineTo(finX,finY);
+    context.stroke();
+};
+var drawArc = function(centerX, centerY, radius, startingAngle, endingAngle, counterclockwise){
+    context.beginPath();
+    context.arc(centerX, centerY, radius, startingAngle, endingAngle, counterclockwise);
+    context.stroke();
+};
+
+var drawRect = function(iniX, iniY, width, height){
+    context.beginPath();
+    context.rect(iniX, iniY, width, height);
+    context.stroke();
+};
+
+
+var fillRect = function(iniX, iniY, width, height){
+    context.beginPath();
+    context.rect(iniX, iniY, width, height);
+    context.fill();
+    context.stroke();
+};
+
+var drawCircle = function(centerX, centerY, radius){
+    drawArc(centerX, centerY, radius, 0, Math.PI*2,false);
+};
+
+
+var fillCircle = function(centerX, centerY, radius){
+    context.beginPath();
+    context.arc(centerX, centerY, radius, 0, Math.PI*2, false);
+    context.fill();
+    context.stroke();
+};
+
+var fillArc = function(centerX, centerY, radiusX, radiusY, startingAngle, endingAngle){
+    context.save();
+    context.beginPath();
+    if(radiusX>radiusY){
+        context.scale(1, radiusY/radiusX);
+        context.arc(centerX, (centerY*radiusX)/radiusY, radiusX, startingAngle, endingAngle, false);
+        context.lineTo(centerX,(centerY*radiusX)/radiusY);
+    }else{
+        context.scale(radiusX/radiusY, 1);
+        context.arc((centerX*radiusY)/radiusX, centerY, radiusY, startingAngle, endingAngle, false);
+        context.lineTo((centerX*radiusY)/radiusX, centerY);
+    }
+    context.closePath();
+    context.fill();
+    context.stroke();
+    context.restore();
+}
